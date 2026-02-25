07d78ff4f74be4db20bd3c260596147c5886893f
Linus Juni <linus@juni.dk>
2026-02-25 16:19:06 +0100

    adds a placeholder for part B

--- miniproject1/src/dataset.py
+++ miniproject1/src/dataset.py
@@ -5,7 +5,7 @@ from src.utils.settings import settings
 
 
 def get_binarized_mnist(train: bool = True) -> Dataset:
-    """Binarized MNIST dataset (pixels > 0.5 become 1, else 0)."""
+    """Binarized MNIST dataset (pixels > 0.5 become 1, else 0). For Part A."""
     return datasets.MNIST(
         settings.DATA_DIR,
         train=train,
@@ -17,3 +17,8 @@ def get_binarized_mnist(train: bool = True) -> Dataset:
             ]
         ),
     )
+
+
+def get_dequantized_mnist(train: bool = True) -> Dataset:
+    """Dequantized MNIST scaled to [-1, 1]. For Part B."""
+    raise NotImplementedError
